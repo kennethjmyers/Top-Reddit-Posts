1ca54c1fd512d9f6fa0e88225ad68f13c678d418
Ken <kennjamesmyers@gmail.com>
2023-04-07 15:49:39 -0400

    Update for zipping lambda function

---
+++ scripts/zipLambdaFunction.sh
@@ -0,0 +1,25 @@
+# This is meant to zip a lambda function with the reddit config
+# use: sh zipLambdaFunction.sh -f someFunction
+# saves zip to zippedLambdaFunction/someFunction.zip
+
+# based on https://www.linkedin.com/pulse/add-external-python-libraries-aws-lambda-using-layers-gabe-olokun/
+while getopts f: flag
+do
+    case "${flag}" in
+        f) function_name=${OPTARG};;  # ie someFunction located in ../lambdaFunction/someFunction
+    esac
+done
+: ${function_name:?Missing -f}   # checks if these have been set https://unix.stackexchange.com/questions/621004/bash-getopts-mandatory-arguments
+echo "lambda function: $function_name";
+
+[ -d "../lambdaFunctions/${function_name}" ] && echo "Directory ../lambdaFunctions/${function_name} exists." || { echo "Error: Directory ../lambdaFunctions/${function_name} does not exist."; exit 1; }
+
+cd ./zippedLambdaFunction/
+rm -r ./${function_name}
+cp -r ../../lambdaFunctions/${function_name} ./
+cp ../../reddit.cfg ./${function_name}/
+rm -rf ${function_name}.zip # remove first if it exists
+cd ./${function_name}/  # for some reason you have to zip from within this folder or it wont work, it otherwise wraps it in another folder
+zip -r ../${function_name}.zip *    # zip of function and the reddit config
+cd ..
+rm -r ./${function_name}  # clean up unzipped file
--- scripts/zipPythonPackage.sh
+++ scripts/zipPythonPackage.sh
@@ -1,39 +1,40 @@
 # This is a script to download a python package binaries, zip it, and upload it to an s3 bucket.
 # The intention is to use that package as a layer for a lambda function (or something else).
-# use: sh packagePython.sh -p praw -s [bucket name] -u [sso name]
+# use: sh zipPythonPackage.sh -p praw -s [bucket name] -u [sso name]
 # might need to login to your sso session first with: aws sso login --profile [sso name]
 
 # based on https://www.linkedin.com/pulse/add-external-python-libraries-aws-lambda-using-layers-gabe-olokun/
 while getopts p:s:u: flag
 do
     case "${flag}" in
-        p) package=${OPTARG};;  # ie praw=7.7.0
+        p) package=${OPTARG};;  # ie praw==7.7.0
         s) s3_bucket=${OPTARG};;  # ie s3://your-s3-bucket-name/
         u) aws_profile=${OPTARG};; # ie AdministratorAccess, think sso username
     esac
 done
-: ${package:?Missing -p} ${s3_bucket:?Missing -s} ${aws_profile:?Missing -s}  # checks if these have been set https://unix.stackexchange.com/questions/621004/bash-getopts-mandatory-arguments
+: ${package:?Missing -p} ${s3_bucket:?Missing -s} ${aws_profile:?Missing -u}  # checks if these have been set https://unix.stackexchange.com/questions/621004/bash-getopts-mandatory-arguments
 echo "package: $package";
-echo "AWS Location: $awslocation";
+echo "S3 Location: s3://$s3_bucket";
+echo "sso profile: $aws_profile"
 
 bucketstatus=$(aws s3api head-bucket --bucket "${s3_bucket}" --profile "${aws_profile}" 2>&1)
 if echo "${bucketstatus}" | grep 'Not Found';
 then
-  echo "bucket doesn't exist";
+  { echo "bucket doesn't exist"; exit 1; }
 elif echo "${bucketstatus}" | grep 'Forbidden';
 then
-  echo "Bucket exists but not owned"
+  { echo "Bucket exists but not owned"; exit 1; }
 elif echo "${bucketstatus}" | grep 'Bad Request';
 then
-  echo "Bucket name specified is less than 3 or greater than 63 characters"
+  { echo "Bucket name specified is less than 3 or greater than 63 characters"; exit 1; }
 else
   echo "Bucket owned and exists";
 fi
 # probably need something that exits if it doesn't exist
 
-mkdir -p ./zippedPackages/${package}/python
+mkdir -p ./zippedPythonPackages/${package}/python
 
-cd ./zippedPackages/${package}/python
+cd ./zippedPythonPackages/${package}/python
 
 # install binaries for package
 pip install \
