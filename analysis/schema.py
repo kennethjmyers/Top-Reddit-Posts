f55db544ea9f61f271665a3d91a621f514a56bc6
Ken <kennjamesmyers@gmail.com>
2023-04-10 17:36:50 -0400

    data cleaning and analysis

---
+++ analysis/schema.py
@@ -0,0 +1,35 @@
+# Contains schemas needed for reading data into spark
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, TimestampType, FloatType
+from datetime import datetime
+
+# functions to convert data on read
+fromDynamoConversion = {
+  'loadDateUTC': lambda x: datetime.strptime(x, '%Y-%d-%m'),
+  'loadTimeUTC': lambda x: datetime.strptime(x, '%H:%M:%S'),
+  'loadTSUTC': lambda x: datetime.strptime(x, '%Y-%d-%m %H:%M:%S'),
+  'postId': str,
+  'subreddit': str,
+  'title': str,
+  'createdTSUTC': lambda x: datetime.strptime(x, '%Y-%d-%m %H:%M:%S'),
+  'timeElapsedMin': int,
+  'score': int,
+  'numComments': int,
+  'upvoteRatio': float,
+  'numGildings': int
+}
+
+# https://sparkbyexamples.com/pyspark/different-ways-to-create-dataframe-in-pyspark/
+toSparkSchema = StructType([
+  StructField("loadDateUTC",TimestampType(),False),
+  StructField("loadTimeUTC",TimestampType(),False),
+  StructField("loadTSUTC",TimestampType(),False),
+  StructField("postId", StringType(), False),
+  StructField("subreddit", StringType(), False),
+  StructField("title", StringType(), False),
+  StructField("createdTSUTC", TimestampType(), False),
+  StructField("timeElapsedMin", IntegerType(), True),
+  StructField("score", IntegerType(), True),
+  StructField("numComments", IntegerType(), True),
+  StructField("upvoteRatio", FloatType(), True),
+  StructField("numGildings", IntegerType(), True),
+])
