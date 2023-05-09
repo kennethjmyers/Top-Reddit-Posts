b5e022a8823ca8c535b0e113dbfd7c368ad0c6c9
Ken <kennjamesmyers@gmail.com>
2023-05-08 21:31:49 -0400

    test lambda update

--- lambdaFunctions/getRedditDataFunction/test_lambda.py
+++ lambdaFunctions/getRedditDataFunction/test_lambda.py
@@ -9,6 +9,8 @@ import os
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(THIS_DIR, '../../'))
 import configUtils as cu
+import pickle
+from moto import mock_dynamodb
 
 
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
@@ -24,7 +26,7 @@ def cfg():
 @pytest.fixture(scope='module')
 def reddit(cfg):
   if IN_GITHUB_ACTIONS:
-    pytest.skip(reason="Config not available in Github Actions.")
+    return pickle.load(open('test_reddit.sav', 'rb'))
   redditcfg = cfg['reddit_api']
   return praw.Reddit(
     client_id=f"{redditcfg['CLIENTID']}",
@@ -35,7 +37,6 @@ def reddit(cfg):
   )
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Config not available in Github Actions.")
 def test_getRedditData(reddit):
   subreddit = "pics"
   ru.getRedditData(
@@ -70,18 +71,21 @@ def test_deduplicateRedditData(duplicatedData):
   print("test_deduplicateRedditData complete")
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Config not available in Github Actions.")
+@mock_dynamodb
 class TestBatchWriter:
+
+
   def classSetUp(self):
     """
     If we left this at top level of the class then it won't be skipped by `skip` and `skipif`
     furthermore we can't have __init__ in a Test Class, so this is called prior to each test
     :return:
     """
-    self.dynamodb_resource = boto3.resource('dynamodb')
-    self.tableName = 'test'
-    self.testRawTableDefinition = tableDefinition.getTableDefinition(self.tableName)
-    self.testTable = ru.getOrCreateTable(self.testRawTableDefinition, self.dynamodb_resource)
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    # create table and write to sample data
+    tableName = 'rising'
+    td = tableDefinition.getTableDefinition(tableName=tableName)
+    self.testTable = dynamodb.create_table(**td)
     self.schema = tableDefinition.schema
     self.columns = self.schema.keys()
     self.Row = namedtuple("Row", self.columns)
@@ -101,7 +105,8 @@ class TestBatchWriter:
           subreddit='pics', title='Magnolia tree blooming in my friends yard', createdTSUTC='2023-04-30 04:19:43',
           timeElapsedMin=44, score=3, numComments=0, upvoteRatio=1.0, numGildings=0)
      ]
-    ru.batchWriter(table=testTable, data=data, schema=schema)
+    from redditUtils import batchWriter
+    batchWriter(table=testTable, data=data, schema=schema)
     print("duplicateDataTester test complete")
 
   def test_uniqueData(self):
@@ -118,7 +123,8 @@ class TestBatchWriter:
           subreddit='pics', title='A piece of wood sticking up in front of a fire.', createdTSUTC='2023-04-30 04:29:23',
           timeElapsedMin=34, score=0, numComments=0, upvoteRatio=0.4, numGildings=0)
     ]
-    ru.batchWriter(table=testTable, data=data, schema=schema)
+    from redditUtils import batchWriter
+    batchWriter(table=testTable, data=data, schema=schema)
     print("uniqueDataTester test complete")
 
   def test_diffPrimaryIndexSameSecondIndex(self):
@@ -135,7 +141,8 @@ class TestBatchWriter:
           subreddit='pics', title='Magnolia tree blooming in my friends yard', createdTSUTC='2023-04-30 04:19:43',
           timeElapsedMin=44, score=3, numComments=0, upvoteRatio=1.0, numGildings=0)
     ]
-    ru.batchWriter(table=testTable, data=data, schema=schema)
+    from redditUtils import batchWriter
+    batchWriter(table=testTable, data=data, schema=schema)
     print("diffPrimaryIndexSameSecondIndexTester test complete")
 
 
